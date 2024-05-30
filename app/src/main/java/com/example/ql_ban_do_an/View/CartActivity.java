8d2a82e2a0d21c533f540c728c2ca835325d8d0d
LeKiet2711 <legiakiet03@gmail.com>
2024-05-30 08:14:24 +0700

    update

--- app/src/main/java/com/example/ql_ban_do_an/View/CartActivity.java
+++ app/src/main/java/com/example/ql_ban_do_an/View/CartActivity.java
@@ -1,10 +1,12 @@
 package com.example.ql_ban_do_an.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ql_ban_do_an.Helper.ManagmentCart;
 import com.example.ql_ban_do_an.R;
@@ -22,10 +24,34 @@ public class CartActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         managementCart=new ManagmentCart(this);
+        caculateCart();
+        initList();
+    }
+
+    private void initList(){
+        if(managementCart.getListCart().isEmpty()){
+            binding.txtEmpty.setVisibility(View.VISIBLE);
+            binding.scrollviewCart.setVisibility(View.GONE);
+        }else{
+            binding.txtEmpty.setVisibility(View.GONE);
+            binding.scrollviewCart.setVisibility(View.VISIBLE);
+        }
+        LinearLayoutManager linearlayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        binding.cartView.setLayoutManager(linearlayoutManager);
 
     }
-    private void caculateCart(){
 
+    private void caculateCart(){
+        double percentTax=0.02;
+        double delivery=10;
+
+        tax=Math.round(managementCart.getTotalFee()*percentTax*100.0)/100;
+        double total=Math.round((managementCart.getTotalFee()+tax+delivery)*100)/100;
+        double itemTotal=Math.round(managementCart.getTotalFee()*100)/100;
+        binding.txtTotalFee.setText("$"+itemTotal);
+        binding.txtTax.setText("$"+tax);
+        binding.txtDelivery.setText("$"+delivery);
+        binding.txtTotal.setText("$"+total);
     }
 
 }

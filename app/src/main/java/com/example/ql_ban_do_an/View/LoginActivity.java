4244e5a50a0ab4bb2a619748b1b6ee820058c6f9
nhatthongcntt <tanbinh131001@gmail.com>
2024-05-30 10:41:33 +0700

    login activity

--- app/src/main/java/com/example/ql_ban_do_an/View/LoginActivity.java
+++ app/src/main/java/com/example/ql_ban_do_an/View/LoginActivity.java
@@ -3,14 +3,21 @@ package com.example.ql_ban_do_an.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ql_ban_do_an.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnSignup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignup = findViewById(R.id.BtnSignup);
+
     }
 }

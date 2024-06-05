9c8d5e7a18163f6eb297e122c9a84d5fdc68b4e5
LeKiet2711 <legiakiet03@gmail.com>
2024-06-05 21:28:51 +0700

    Hiển thị thành công CartActivity

--- app/src/main/java/com/example/ql_ban_do_an/View/MainActivity.java
+++ app/src/main/java/com/example/ql_ban_do_an/View/MainActivity.java
@@ -142,9 +142,6 @@ public class MainActivity extends AppCompatActivity{
 
 
 
-
-
-
     private void initLocation() {
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://qlbandoan-6f252-default-rtdb.asia-southeast1.firebasedatabase.app/");
         DatabaseReference myRef = database.getReference("Location");

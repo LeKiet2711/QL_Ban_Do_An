0a37a06b0cb64da66c605106d34fdf817dfa73df
VoThiKimGiau <vothikimgiau16122003@gmail.com>
2024-06-08 18:49:43 +0700

    Sửa 1 xíu phần cart

--- app/src/main/java/com/example/ql_ban_do_an/Controller/CartAdapter.java
+++ app/src/main/java/com/example/ql_ban_do_an/Controller/CartAdapter.java
@@ -46,9 +46,9 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.viewholder> {
     public void onBindViewHolder(@NonNull CartAdapter.viewholder holder, int position) {
         holder.title.setText(list.get(position).getTitle());
         holder.feeEachItem.setText("$"+list.get(position).getPrice());
-        holder.totalEachItem.setText(list.get(position).getNumberInCart()+"*$"+(
+        holder.totalEachItem.setText(list.get(position).getNumberInCart()+" * $"+(
                  list.get(position).getNumberInCart()*list.get(position).getPrice()));
-        //holder.num.setText(list.get(position).getNumberInCart());
+        holder.num.setText(String.valueOf(list.get(position).getNumberInCart()));
 
         Picasso.with(context)
                 .load(list.get(position).getImagePath())
